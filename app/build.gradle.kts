9a8833501e46a93c9f641cd4c77ff59f3cb67ced
ileshkumar-sketch <ilesh.knkumar@gmail.com>
2026-09-12 13:33:29 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,15 @@ android {
     namespace = "com.shaligramjv.saleentry"
     compileSdk = 35
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     defaultConfig {
         applicationId = "com.shaligramjv.saleentry"
         minSdk = 24
